e773ed3a95e0e720546baca10d56f5daea00e3ee
Patrick Elliott <patrick.d.elliott@gmail.com>
2018-11-09 10:10:53 +0100

    First commit

---
+++ default.nix
@@ -0,0 +1,15 @@
+{ mkDerivation, apecs, base, grid, lens, sdl2, sdl2-image
+, sdl2-mixer, sdl2-ttf, stdenv, text, vector
+}:
+mkDerivation {
+  pname = "doryen-hs";
+  version = "0.1.0.0";
+  src = ./.;
+  isLibrary = false;
+  isExecutable = true;
+  executableHaskellDepends = [
+    apecs base grid lens sdl2 sdl2-image sdl2-mixer sdl2-ttf text
+    vector
+  ];
+  license = stdenv.lib.licenses.bsd3;
+}
